c5b526cd643c38f4564a578a296056755925220f
Jay Packard <jay.packard@gmail.com>
2014-10-23 07:44:55 -0400

    first commit

---
+++ jquery.disabling.js
@@ -0,0 +1,31 @@
+/*!
+ * jQuery Disabling v 1.0.0
+ * https://github.com/jaypackard/jquery.disabling
+ *
+ * Copyright 2014 Jay Packard
+ * Released under the MIT license
+ */
+(function($) {
+  $.fn.disabling = function(options) {
+    var $this = $(this);
+    
+    $this.click(function() {
+      perform($this);
+    });  
+
+    perform($this);
+
+    return $this;
+  };
+
+  function perform($obj) {
+    if ($obj.is(':checked')) {
+      $obj.siblings('[name$="[_destroy]"]').val(0);
+      $obj.siblings().attr('disabled', false);
+    }
+    else {
+      $obj.siblings('[name$="[_destroy]"]').val(1);
+      $obj.siblings().attr('disabled', $obj.siblings('[name$="[id]"]').val() == '' ? false : true);
+    }
+  }
+})(jQuery);
